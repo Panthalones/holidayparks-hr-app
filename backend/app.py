f3f656e395f0be42584771195cb4eb48d22cd56b
Younes El Moussaoui <i559284@fontysict.nl>
2026-06-02 21:13:43 +0200

    CI/CD pipeline test

--- backend/app.py
+++ backend/app.py
@@ -128,7 +128,7 @@ def logout():
 
     return redirect(
         f"{AUTHORITY}/oauth2/v2.0/logout"
-       f"?post_logout_redirect_uri={BACKEND_BASE_URL}/login"
+        "?post_logout_redirect_uri={BACKEND_BASE_URL}/login"
     )
 
 @app.route("/api/health", methods=["GET"])
